1f1794376f66ea61de776dc33594e0f19ba351f5
unknown <alejandraadriantejada@gmail.com>
2021-06-05 06:18:18 -0500

    new port

--- src/config.mjs
+++ src/config.mjs
@@ -4,12 +4,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-  dev: process.env.NODE_ENV !== 'production',
-  port: process.env.PORT || 3001,
-  mongo: {
-    uri: process.env.MONGO_URI,
-    db: process.env.MONGO_DB
-  }
+    dev: process.env.NODE_ENV !== 'production',
+    port: process.env.PORT || 3000,
+    mongo: {
+        uri: process.env.MONGO_URI,
+        db: process.env.MONGO_DB
+    }
 }
 
 export { config }
